67c9b3fe5a9affed996db1a5185e29983f662e65
DionysiosAngelopoulos <manco147@yahoo.com>
2017-06-11 13:05:19 +0300

    Improved world generator and properly commented it

--- Space-World-Simulation/Sector.cs
+++ Space-World-Simulation/Sector.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Space_World_Simulation {
     class Sector {
-        public Sector[] connections;	// Our connections to other sectors.
+        public List<Sector> connections;	// Our connections to other sectors.
         public List<Unit> units;		// The units contained within this sector.
 		public World world { get; private set; }
 
@@ -15,6 +15,7 @@ namespace Space_World_Simulation {
 
 		public Sector(string name, World world) {
 			this.units = new List<Unit>();
+			this.connections = new List<Sector>();
 			this.world = world;
 			this.name  = name;
 		}
--- Space-World-Simulation/World.cs
+++ Space-World-Simulation/World.cs
@@ -29,25 +29,33 @@ namespace Space_World_Simulation {
 
 		public void Generate(Sector initSector, int size) {
 			Random rand = new Random();
-			int x = 1;
+			int x = 1;	// Sectors are named f0, f1, f2, f3... for now. This variable
+						// is used to name them. It is also used to count how many sectors
+						// were generated.
+						// FIXME: Fix this
 
-			List<Sector> frontier = new List<Sector>();
+			List<Sector> frontier = new List<Sector>();	// We use a BFS essentitally to generate the world.
+														// we add a sector to the frontier and generate some
+														// neighbours for it. Then we add the neighbours to
+														// the frontier and give them some neighbours. We
+														// stop when the size variable reaches size.
 			frontier.Add(initSector);
-			Sector current;
+			Sector current;				// The sector we are currently processing.
 			while(true) {
 				current = frontier[0];
-				int len = rand.Next(1, 9);
-				current.connections = new Sector[len];
+				int len = rand.Next(1, 9);  // How many connections the sector will have.
 				for (int i = 0; i < len; i++) {
-					current.connections[i] = new Sector("s" + x, this);
-					frontier.Add(current.connections[i]);
-					sectors.Add(current.connections[i]);
+					Sector s = new Sector("f" + x, this);   // Make a new neighbour.
+					current.connections.Add(s);				// Add the new neighbour to our connections.
+					current.connections[current.connections.IndexOf(s)].connections.Add(current);   // Add ourselves to our neighbours connections.
+					frontier.Add(current.connections[current.connections.IndexOf(s)]);	// Add the neighbour to the frontier.
+					sectors.Add(current.connections[current.connections.IndexOf(s)]);   // Add the neighbour to the sectors list.
+					if (x >= size) {	// Check if enough sectors have been created.
+						return;
+					}
+					x += 1;	// Increase x.
 				}
-				frontier.Remove(current);
-				x++;
-				if (x > size) {
-					break;
-				}
+				frontier.Remove(current);	// Remove ourselves from the frontier.
 			}
 		}
 	}
